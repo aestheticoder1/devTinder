34f3e54468e4088aa3d3dad8dd02645f057c10b7
Nihal Rawat <nihal.riya03@gmail.com>
2025-02-10 16:03:18 +0530

    Resolve Error

--- src/app.js
+++ src/app.js
@@ -38,6 +38,6 @@ connectDB()
             console.log('Server is running on port 7777');
         });
     }).catch(err => {
-        console.error("Error connecting to MongoDB:");
+        console.error("Error connecting to MongoDB:",+err.message);
     })
 
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     await mongoose.connect(
-        process.env.DB_CONNECTION_SECRET
+        "mongodb+srv://aestheticoder:Um%40rawat12@namastenode.cpswe.mongodb.net/devTinder"
     );
 }
 
